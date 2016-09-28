0236cd10a2ce131394f9184b896cf4b81c9edbab
Pepos <peposcarmona@gmail.com>
2016-09-27 19:54:56 -0500

    Anadidos comentarios

--- assign1/storage_mgr.c
+++ assign1/storage_mgr.c
@@ -41,7 +41,7 @@ void initStorageManager(){
  *    2016-09-24  Victor Portals   <vportalslorenzo@hawk.iit.edu>   Error handle.
  *    2016-09-26  Sergio Penavades <spenavadessuarez@hawk.iit.edu>  Add header comment,
  *                                                                  add comments.
- *    2016-09-27  Jose Carmona     <jcarmonalopez@hawk.iit.edu>     Fixed error handle..
+ *    2016-09-27  Jose Carmona     <jcarmonalopez@hawk.iit.edu>     Fixed error handle.
 **************************************************************************************************/
 RC createPageFile (char *fileName){
   FILE *file = fopen(fileName, "w");
@@ -52,12 +52,17 @@ RC createPageFile (char *fileName){
     fileInfo.totalNumPages = 1;
     fileInfo.curPagePos = 0;
 
+    // SM_FileHeader struct will be stored at the start of the document
+    // It will only contains totalNumPages and curPagePos
+
     errno = 0;
     fwrite(&fileInfo, sizeof(SM_FileHeader), 1, file);
     if(errno != 0){
       fclose(file);
       return RC_WRITE_FAILED;
     }
+
+    // We writes the first page full of '\0'
     for (int i = 0; i < PAGE_SIZE; ++i){
       errno = 0;
       fwrite(&eof, sizeof(eof), 1, file);
@@ -66,6 +71,8 @@ RC createPageFile (char *fileName){
         return RC_WRITE_FAILED;
       }
     }
+
+    // file is closed
     errno = 0;
     fclose(file);
     if(errno == 0)
@@ -102,16 +109,24 @@ RC createPageFile (char *fileName){
  *                                                                  add comments.
 **************************************************************************************************/
 RC openPageFile (char *fileName, SM_FileHandle *fHandle){
+
+  // file is opened
   FILE *file = fopen(fileName, "r+");
   struct SM_FileHeader fileInfo;
+
   if (file){
+    // we read first the metadata at the start of the file
     errno = 0;
     fread(&fileInfo, sizeof(SM_FileHeader), 1, file);
     if(errno != 0) return RC_FILE_HANDLE_NOT_INIT;
+
+    // now we initialize the SM_FileHandle struct
     fHandle->fileName = fileName;
     fHandle->totalNumPages = fileInfo.totalNumPages;
     fHandle->curPagePos = fileInfo.curPagePos;
     fHandle->mgmtInfo = file;
+    // we are using mgmtInfo to store the file handle needed
+    // to write, read, close and other I/O operations
     return RC_OK;
   }else{
     return RC_FILE_NOT_FOUND;
@@ -204,14 +219,21 @@ RC destroyPageFile (char *fileName){
  *                                                                  add comments.
 **************************************************************************************************/
 RC readBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage){
+  // Checks if pageNum is inside the bounds
   if(pageNum < 0 || pageNum >= fHandle->totalNumPages)
     return RC_READ_NON_EXISTING_PAGE;
+
+  // we use fseek to move the read pointer of our file
+  // where we want to start reading
   errno = 0;
   fseek(fHandle->mgmtInfo, sizeof(SM_FileHeader)+pageNum*PAGE_SIZE, SEEK_SET);
   if(errno != 0) return RC_FILE_HANDLE_NOT_INIT;
   errno = 0;
   fread(memPage, PAGE_SIZE, 1, fHandle->mgmtInfo);
   if(errno != 0) return RC_FILE_HANDLE_NOT_INIT;
+
+  // this function will update metadata in fHandle struct and
+  // it will write the metadata to the file
   updateCurrentInformation(fHandle->totalNumPages, pageNum, fHandle);
   return RC_OK;
 };
@@ -233,10 +255,10 @@ RC readBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage){
  * History:
  *    Date        Name                                              Content
  *    ----------  ------------------------------------------------  ------------------------------
- *    2016-09-20  Jose Carmona     <jcarmonalopez@hawk.iit.edu>     Initialization, main logic..
- *    2016-09-21  Jose Carmona     <jcarmonalopez@hawk.iit.edu>     Main logic..
+ *    2016-09-20  Jose Carmona     <jcarmonalopez@hawk.iit.edu>     Initialization.
+ *    2016-09-21  Jose Carmona     <jcarmonalopez@hawk.iit.edu>     Main logic.
  *    2016-09-26  Sergio Penavades <spenavadessuarez@hawk.iit.edu>  Error handle. 
- *    2016-09-26  Sergio Penavades <spenavadessuarez@hawk.iit.edu>  dd header comment,
+ *    2016-09-26  Sergio Penavades <spenavadessuarez@hawk.iit.edu>  Add header comment,
  *                                                                  add comments.
 **************************************************************************************************/
 int getBlockPos (SM_FileHandle *fHandle){
@@ -269,6 +291,7 @@ int getBlockPos (SM_FileHandle *fHandle){
  *                                                                  add comments.
 **************************************************************************************************/
 RC readFirstBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
+  // first block has pageNum = 0
   return readBlock(0, fHandle, memPage);
 };
 
@@ -296,6 +319,8 @@ RC readFirstBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
  *                                                                  add comments.
 **************************************************************************************************/
 RC readLastBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
+  // Last block has pageNum = totalNumPages
+  // readBlock function will check if pageNum is inside the bounds
   return readBlock(fHandle->totalNumPages-1, fHandle, memPage);
 };
 
@@ -323,6 +348,8 @@ RC readLastBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
  *                                                                  add comments.
 **************************************************************************************************/
 RC readPreviousBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
+  // Previous block has pageNum = curPagePos-1
+  // readBlock function will check if pageNum is inside the bounds
   return readBlock(fHandle->curPagePos-1, fHandle, memPage);
 };
 
@@ -350,6 +377,8 @@ RC readPreviousBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
  *                                                                  add comments.
 **************************************************************************************************/
 RC readCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
+  // Current block has pageNum = curPagePos
+  // readBlock function will check if pageNum is inside the bounds
   return readBlock(fHandle->curPagePos, fHandle, memPage);
 };
 
@@ -377,6 +406,8 @@ RC readCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
  *                                                                  add comments.
 **************************************************************************************************/
 RC readNextBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
+  // Next block has pageNum = curPagePos+1
+  // readBlock function will check if pageNum is inside the bounds
   return readBlock(fHandle->curPagePos+1, fHandle, memPage);
 };
 
@@ -407,11 +438,17 @@ RC readNextBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
  *    2016-09-27  Jose Carmona     <jcarmonalopez@hawk.iit.edu>     Fixed error handle.
 **************************************************************************************************/
 RC writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage){
+  // Checks if pageNum is inside the bounds
   if(pageNum < 0 || pageNum >= fHandle->totalNumPages)
     return RC_READ_NON_EXISTING_PAGE;
+
+  // we use fseek to move the read pointer of our file
+  // where we want to start writing
   errno = 0;
   fseek(fHandle->mgmtInfo, sizeof(SM_FileHeader)+pageNum*PAGE_SIZE, SEEK_SET);
   if(errno != 0) return RC_FILE_HANDLE_NOT_INIT;
+
+
   errno = 0;
   fwrite(memPage, PAGE_SIZE, 1, fHandle->mgmtInfo);
   if(errno != 0) return RC_WRITE_FAILED;
@@ -442,6 +479,8 @@ RC writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage){
  *                                                                  add comments.
 **************************************************************************************************/
 RC writeCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
+  // Current block has pageNum = curPagePos
+  // readBlock function will check if pageNum is inside the bounds
   return writeBlock(fHandle->curPagePos, fHandle, memPage);
 };
 
@@ -469,19 +508,27 @@ RC writeCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
  *                                                                  add comments.
 **************************************************************************************************/
 RC appendEmptyBlock (SM_FileHandle *fHandle){
+  // the file is opened as 'r+'
+  // we need to close and reopen as 'a'
   fclose(fHandle->mgmtInfo);
   FILE *file = fopen(fHandle->fileName, "a");
   if(file){
+
     char eof = '\0';
+    // we append a new page full of '\0' character
     for(int i = 0; i < PAGE_SIZE; i++){
       errno = 0;
       fwrite(&eof, sizeof(eof), 1, file);
       if(errno != 0) return RC_WRITE_FAILED;
     }
+
+    // close file
     errno = 0;
     fclose(file);
     if(errno != 0) return RC_WRITE_FAILED;
+    // we reopen again as 'r+'
     openPageFile(fHandle->fileName, fHandle);
+    // we need to update metadata information, totalNumPages has been increased
     updateCurrentInformation(fHandle->totalNumPages+1, fHandle->curPagePos, fHandle);
     return RC_OK;
   }else{
@@ -514,7 +561,9 @@ RC appendEmptyBlock (SM_FileHandle *fHandle){
  *                                                                  add comments.
 **************************************************************************************************/
 RC ensureCapacity (int numberOfPages, SM_FileHandle *fHandle){
+  // how many pages we need to append
   int numberToEnsure = numberOfPages - fHandle->totalNumPages;
+  // if the number is negative or 0, we dont need to append any page
   if(numberToEnsure > 0){
     for(int i = 0; i < numberToEnsure; i++){
       RC res = appendEmptyBlock(fHandle);
@@ -551,11 +600,16 @@ RC ensureCapacity (int numberOfPages, SM_FileHandle *fHandle){
 **************************************************************************************************/
 
 RC updateCurrentInformation(int totalNumPages, int curPagePos, SM_FileHandle *fHandle){
+  // fHandle is updated
   fHandle->totalNumPages = totalNumPages;
   fHandle->curPagePos = curPagePos;
+
+  // a SM_FileHeader struct is created to be written to the file
   struct SM_FileHeader fileInfo;
   fileInfo.totalNumPages = fHandle->totalNumPages;
   fileInfo.curPagePos = fHandle->curPagePos;
+
+  // we want to write at the start of the file
   errno = 0;
   fseek(fHandle->mgmtInfo, 0, SEEK_SET);
   if(errno != 0) return RC_FILE_HANDLE_NOT_INIT;
