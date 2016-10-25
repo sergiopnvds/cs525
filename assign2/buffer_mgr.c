58a8622ad3fdbda52f3aadb734eff54c9382019f
vportalslorenzo <vportalslorenzo@hawk.iit.edu>
2016-10-25 00:07:18 -0500

    Implemented buffer FIFO and LRU. Test passed

--- assign2/buffer_mgr.c
+++ assign2/buffer_mgr.c
@@ -8,55 +8,39 @@
 #include <string.h>
 #include <unistd.h>
 
-extern int errno;
-
-
-
-typedef struct BM_Mgmtdata{
-	SM_FileHandle fileHandle;
-	void* buffer;
-} BM_Mgmtdata;
-
-typedef struct BM_FIFOBuffer{
-	SM_PageHandle* frameBuffer;
-	int* pageIndex;
-	char* dirtyFlags;
-	int insertPos;
-} BM_FIFOBuffer;
-
-void createFIFOBuffer(BM_FIFOBuffer* buffer, const int numFrames);
-void insertFIFO(BM_BufferPool *const bm, BM_PageHandle *ph);
-void removeFIFO(BM_BufferPool *const bm, PageNumber pageNum);
-int findPageIndexFIFO(int numPage, int totalPages, int *pageIndex);
-RC markDirtyFIFO(BM_BufferPool *const bm, BM_PageHandle *const page);
-void forcePageFIFO (BM_BufferPool *const bm, int pageNum);
-
 RC initBufferPool(BM_BufferPool *const bm, const char *const pageFileName, const int numPages, ReplacementStrategy strategy, void *stratData){
 	SM_FileHandle fileHandle;
 	char *pageFileName2 = strdup(pageFileName);
 	if(openPageFile (pageFileName2, &fileHandle) == RC_FILE_NOT_FOUND) {
 		return RC_FILE_NOT_FOUND;
 	} else {
-		void* buffer;
-		switch(strategy){
-			case RS_FIFO:
-				buffer = malloc(sizeof(BM_FIFOBuffer));
-				createFIFOBuffer(buffer, numPages);
-				break;
-			case RS_LRU:
-				break;
-			case RS_CLOCK:
-				break;
-			case RS_LFU:
-				break;
-			case RS_LRU_K:
-				break;
-			default:
-				return -1;
+		BM_Buffer *buffer = malloc(sizeof(BM_Buffer));
+		SM_PageHandle *frameBuffer = malloc(PAGE_SIZE * numPages);//*
+		PageNumber *pageIndex = malloc(sizeof(int)*numPages);//*
+		bool *dirtyFlags = malloc(sizeof(bool)*numPages);//*
+		int *fixCount = malloc(sizeof(int)*numPages);//*
+		long *lastUseTime = malloc(sizeof(long)*numPages);
+
+		for(int i = 0; i < numPages;i++){//*
+			pageIndex[i] = NO_PAGE;//*
+			dirtyFlags[i] = FALSE;//*
+			fixCount[i] = 0;//*
+			lastUseTime[i] = -1; 
 		}
+
+		buffer->frameBuffer = frameBuffer;//*
+		buffer->pageIndex = pageIndex;//*
+		buffer->dirtyFlags = dirtyFlags;//*
+		buffer->fixCount = fixCount;//*
+		buffer->insertPos = 0;
+		buffer->timeCounter = 0;
+		buffer->lastUseTime = lastUseTime;
+
 		BM_Mgmtdata *mgmtData = malloc(sizeof(BM_Mgmtdata));
 		mgmtData->fileHandle = fileHandle;
 		mgmtData->buffer = buffer;
+		mgmtData->numReadIO = 0;
+		mgmtData->numWriteIO = 0;
 		// Write BufferPool data and fifo buffer to mgmtData
 		bm->pageFile = pageFileName2;
 		bm->numPages = numPages;
@@ -64,239 +48,146 @@ RC initBufferPool(BM_BufferPool *const bm, const char *const pageFileName, const
 		bm->mgmtData = mgmtData;
 		return RC_OK;
 	}
-
-
 };
 RC shutdownBufferPool(BM_BufferPool *const bm){
-	/*TODO*/
-	return RC_OK;
+	return forceFlushPool(bm);
 };
+
 RC forceFlushPool(BM_BufferPool *const bm){
-	printf("Flushing pool\n");
 	BM_Mgmtdata *mgmtData = bm->mgmtData;
-	BM_FIFOBuffer *fifoBuffer = mgmtData->buffer;
+	BM_Buffer *LRUBuffer = mgmtData->buffer;
 	for(int i=0;i < bm->numPages;i++){
-		if(fifoBuffer->dirtyFlags[i] == 0x01)
-			printf("Flushing page %i\n", fifoBuffer->pageIndex[i]);
-			writeBlock (fifoBuffer->pageIndex[i], &mgmtData->fileHandle, fifoBuffer->frameBuffer[i]);
+		if(LRUBuffer->dirtyFlags[i] == TRUE){
+			int err = writeBlock (LRUBuffer->pageIndex[i], &mgmtData->fileHandle, LRUBuffer->frameBuffer[i]);
+			if(err != RC_OK) return err;
+			LRUBuffer->dirtyFlags[i] = FALSE;
+			mgmtData->numWriteIO++;
+		}
 	}
-	/*TODO*/
 	return RC_OK;
-};
+}
 
 // Buffer Manager Interface Access Pages
 RC markDirty (BM_BufferPool *const bm, BM_PageHandle *const page){
-	switch(bm->strategy){
-		case RS_FIFO:
-			return markDirtyFIFO(bm, page);
-			break;
-		case RS_LRU:
-			break;
-		case RS_CLOCK:
-			break;
-		case RS_LFU:
-			break;
-		case RS_LRU_K:
-			break;
-		default:
-			return -1;
-	}
-	return RC_OK;
-};
-RC unpinPage (BM_BufferPool *const bm, BM_PageHandle *const page){
-	//TODO: Force Page if marked dirty?
-	switch(bm->strategy){
-		case RS_FIFO:
-			removeFIFO(bm, page->pageNum);
-			break;
-		case RS_LRU:
-			break;
-		case RS_CLOCK:
-			break;
-		case RS_LFU:
-			break;
-		case RS_LRU_K:
-			break;
-		default:
-			return -1;
-	}
-	return RC_OK;
-};
-RC forcePage (BM_BufferPool *const bm, BM_PageHandle *const page){
-	switch(bm->strategy){
-		case RS_FIFO:
-			forcePageFIFO(bm,page->pageNum);
-			break;
-		case RS_LRU:
-			break;
-		case RS_CLOCK:
-			break;
-		case RS_LFU:
-			break;
-		case RS_LRU_K:
-			break;
-		default:
-			return -1;
-	}
-	return RC_OK;
-};
-RC pinPage (BM_BufferPool *const bm, BM_PageHandle *const page, const PageNumber pageNum){
-	
 	BM_Mgmtdata *mgmtData = bm->mgmtData;
-	// Read file block to memPage
-	SM_PageHandle memPage = malloc(PAGE_SIZE);
-	readBlock (pageNum, &mgmtData->fileHandle, memPage);
-
-	// Stores page data and number in BM_PageHandle
-	page->pageNum = pageNum;
-	page->data = memPage;
-
-	switch(bm->strategy){
-		case RS_FIFO:
-			insertFIFO(bm,page);
-			break;
-		case RS_LRU:
-			break;
-		case RS_CLOCK:
-			break;
-		case RS_LFU:
-			break;
-		case RS_LRU_K:
-			break;
-		default:
-			return -1;
-	} 
+	BM_Buffer *LRUBuffer = mgmtData->buffer;
+	int index = findPageIndex(page->pageNum, bm->numPages,LRUBuffer->pageIndex);
+	LRUBuffer->dirtyFlags[index] = TRUE;
 	return RC_OK;
+}
+
+RC unpinPage (BM_BufferPool *const bm, BM_PageHandle *const page){
+	BM_Mgmtdata *mgmtData = bm->mgmtData;
+	BM_Buffer *fifoBuffer = mgmtData->buffer;
+
+	int index = findPageIndex(page->pageNum, bm->numPages,fifoBuffer->pageIndex);
+	if(index != NO_PAGE) fifoBuffer->fixCount[index]--;
+
+	return RC_OK;
+}
+
+RC forcePage (BM_BufferPool *const bm, BM_PageHandle *const page){
+	BM_Mgmtdata *mgmtData = bm->mgmtData;
+	BM_Buffer *LRUBuffer = mgmtData->buffer;
+	int index = findPageIndex(page->pageNum, bm->numPages,LRUBuffer->pageIndex);
+	int err = writeBlock (page->pageNum, &mgmtData->fileHandle, LRUBuffer->frameBuffer[index]);
+	if(err != RC_OK) return err;
+	mgmtData->numWriteIO++;
+	LRUBuffer->dirtyFlags[index] = FALSE;
+	return RC_OK;
+}
+
+RC pinPage (BM_BufferPool *const bm, BM_PageHandle *const page, const PageNumber pageNum){
+	BM_Mgmtdata *mgmtData = bm->mgmtData;
+	BM_Buffer *buffer = mgmtData->buffer;
+
+	int index = findPageIndex(pageNum, bm->numPages,buffer->pageIndex);
+	if(index == NO_PAGE){
+		int insertionIndex = -1;
+		if(bm->strategy == RS_LRU){
+			insertionIndex = searchLowerTime(buffer->lastUseTime, buffer->fixCount, bm->numPages);
+			if(insertionIndex < 0) return RC_OK;
+		}else if(bm->strategy == RS_FIFO){
+			insertionIndex = buffer->insertPos;
+			if(buffer->fixCount[insertionIndex] > 0){
+				buffer->insertPos = (insertionIndex + 1)%bm->numPages;
+				return pinPage(bm, page, pageNum);
+			}
+		}
+		if(buffer->dirtyFlags[insertionIndex] == TRUE){
+			int err = writeBlock (buffer->pageIndex[insertionIndex], &mgmtData->fileHandle, buffer->frameBuffer[insertionIndex]);
+			if(err != RC_OK) return err;
+			mgmtData->numWriteIO++;
+		}
+		if((mgmtData->fileHandle).totalNumPages <=  pageNum){
+			ensureCapacity(pageNum + 1, &mgmtData->fileHandle);
+		}
+		SM_PageHandle memPage = malloc(PAGE_SIZE);
+		RC code = readBlock (pageNum, &mgmtData->fileHandle, memPage);
+		if(code != RC_OK) return code;
+		mgmtData->numReadIO++;
+		page->pageNum = pageNum;
+		page->data = memPage;
+
+		buffer->frameBuffer[insertionIndex] = page->data;
+		buffer->pageIndex[insertionIndex] = page->pageNum;
+		buffer->dirtyFlags[insertionIndex] = FALSE;
+		buffer->fixCount[insertionIndex] = 1;
+		buffer->insertPos = (insertionIndex + 1)%bm->numPages;
+		buffer->lastUseTime[insertionIndex] = buffer->timeCounter++;
+	}else{
+		page->pageNum = pageNum;
+		page->data = buffer->frameBuffer[index];
+		buffer->fixCount[index]++;
+		if(bm->strategy == RS_LRU) buffer->lastUseTime[index] = buffer->timeCounter++;
+	}
+	return RC_OK;
+
 };
 
 // Statistics Interface
 PageNumber *getFrameContents (BM_BufferPool *const bm){
-	return RC_OK;
-};
+	BM_Mgmtdata *mgmtData = bm->mgmtData;
+	BM_Buffer *LRUBuffer = mgmtData->buffer;
+	return (PageNumber *)LRUBuffer->pageIndex;
+}
+
 bool *getDirtyFlags (BM_BufferPool *const bm){
-	bool *temp = malloc(sizeof(*temp));
-    *temp=true;
-	return temp;
-};
+	BM_Mgmtdata *mgmtData = bm->mgmtData;
+	BM_Buffer *LRUBuffer = mgmtData->buffer;
+	return LRUBuffer->dirtyFlags;
+}
+
 int *getFixCounts (BM_BufferPool *const bm){
-	int *temp = malloc(sizeof(*temp));
-    *temp = 0;
-	return temp;
-};
+	BM_Mgmtdata *mgmtData = bm->mgmtData;
+	BM_Buffer *LRUBuffer = mgmtData->buffer;
+	return LRUBuffer->fixCount;
+}
+
 int getNumReadIO (BM_BufferPool *const bm){
-	return -1;
+	BM_Mgmtdata *mgmtData = bm->mgmtData;
+	return mgmtData->numReadIO;
 };
 int getNumWriteIO (BM_BufferPool *const bm){
-	return -1;
+	BM_Mgmtdata *mgmtData = bm->mgmtData;
+	return mgmtData->numWriteIO;
 };
 
-void createFIFOBuffer(BM_FIFOBuffer* fifoBuffer, const int numFrames){
-	printf("Creating FIFO Buffer\n");
-	// We create a fifoBuffer structure
-	// We create buffer structure to allocate frames as BM_PageHandle structures.
-	SM_PageHandle *frameBuffer = malloc(PAGE_SIZE * numFrames);
-	// We create index array of same size as buffer to allocate info for replacement. 
-	int *pageIndex = malloc(sizeof(int)*numFrames);
-	char *dirtyFlags = malloc(sizeof(char)*numFrames);
-	// Initializes index array values to -1
-	for(int i = 0; i < numFrames;i++){
-		pageIndex[i] = -1;
-		dirtyFlags[i] = 0x00;
-	}
-	for(int i = 0; i < numFrames;i++){
-		printf("%i,", pageIndex[i]);
-	}
-	printf("\n");
-	// Write buffer and index arrays to fifo structure. First inserting position is 0.
-	fifoBuffer->frameBuffer = frameBuffer;
-	fifoBuffer->pageIndex = pageIndex;
-	fifoBuffer->dirtyFlags = dirtyFlags;
-	fifoBuffer->insertPos = 0;
-
-	return;
-}
-
-void insertFIFO(BM_BufferPool *const bm, BM_PageHandle *ph){
-	//TODO: Search for empty space  
-	printf("insertFIFO\n");
-	BM_Mgmtdata *mgmtData = bm->mgmtData;
-	BM_FIFOBuffer *fifoBuffer = mgmtData->buffer;
-
-	int index = findPageIndexFIFO(ph->pageNum, bm->numPages,fifoBuffer->pageIndex);
-
-	printf("Inserting page %i\n", ph->pageNum);
-	if(index < 0){
-		if(fifoBuffer->pageIndex[fifoBuffer->insertPos] >= 0 && fifoBuffer->dirtyFlags[fifoBuffer->insertPos] == 0x01){
-			printf("Flushing page %i\n", fifoBuffer->pageIndex[fifoBuffer->insertPos]);
-			writeBlock (fifoBuffer->pageIndex[fifoBuffer->insertPos], &mgmtData->fileHandle, fifoBuffer->frameBuffer[fifoBuffer->insertPos]);
-		}
-		fifoBuffer->frameBuffer[fifoBuffer->insertPos] = ph->data;
-		fifoBuffer->pageIndex[fifoBuffer->insertPos] = ph->pageNum;
-		fifoBuffer->dirtyFlags[fifoBuffer->insertPos] = 0x00;
-		fifoBuffer->insertPos = (fifoBuffer->insertPos + 1)%bm->numPages;
-	}else{
-		printf("Page is already in buffer, nothing to insert\n");
-	}
-	for(int i = 0; i < bm->numPages;i++){
-		printf("%i,", fifoBuffer->pageIndex[i]);
-	}
-	printf("\n");
-	return;
-}
-
-void removeFIFO(BM_BufferPool *const bm, PageNumber pageNum){
-	// TODO: fill buffer frame with zeroes (?)
-	printf("removeFIFO\n");
-	BM_Mgmtdata *mgmtData = bm->mgmtData;
-	BM_FIFOBuffer *fifoBuffer = mgmtData->buffer;
-
-	printf("Removing page %i from buffer\n",pageNum);
-
-	int index = findPageIndexFIFO(pageNum, bm->numPages,fifoBuffer->pageIndex);
-
-	if(index < 0){
-		printf("Page not found, nothing will be removed\n");
-	}else{
-		if(fifoBuffer->dirtyFlags[index] == 0x01){
-			printf("Flushing page %i\n", fifoBuffer->pageIndex[index]);
-			writeBlock (pageNum, &mgmtData->fileHandle, fifoBuffer->frameBuffer[index]);
-		}
-		fifoBuffer->pageIndex[index] = -1;
-		fifoBuffer->dirtyFlags[index] = 0x00;
-		printf("Page %i removed\n",pageNum);
-	}
-	for(int i = 0; i < bm->numPages;i++){
-		printf("%i,", fifoBuffer->pageIndex[i]);
-	}
-	printf("\n");
-	return;
-}
-
-RC markDirtyFIFO (BM_BufferPool *const bm, BM_PageHandle *const page){
-	printf("Marking page %i as dirty\n", page->pageNum);
-	BM_Mgmtdata *mgmtData = bm->mgmtData;
-	BM_FIFOBuffer *fifoBuffer = mgmtData->buffer;
-	int index = findPageIndexFIFO(page->pageNum, bm->numPages,fifoBuffer->pageIndex);
-	fifoBuffer->dirtyFlags[index] = 0x01;
-	return RC_OK;
-}
-
-void forcePageFIFO (BM_BufferPool *const bm, int pageNum){
-	printf("Write page %i to file.\n", pageNum);
-	BM_Mgmtdata *mgmtData = bm->mgmtData;
-	BM_FIFOBuffer *fifoBuffer = mgmtData->buffer;
-	int index = findPageIndexFIFO(pageNum, bm->numPages,fifoBuffer->pageIndex);
-	writeBlock (pageNum, &mgmtData->fileHandle, fifoBuffer->frameBuffer[index]);
-
-}
-
-int findPageIndexFIFO (int numPage, int totalPages, int *pageIndex){
-	int position = -1;
+int findPageIndex (int numPage, int totalPages, PageNumber *pageIndex){
+	PageNumber position = NO_PAGE;
 	for(int i = 0; i < totalPages && position < 0;i++){
 		if (numPage == pageIndex[i]){
 			position = i;
 		}
 	}
-	//printf("%i\n", position);
 	return position;
 }
+
+int searchLowerTime(long *lastUseTime, int *fixCount, int totalPages){
+	int lowerIndex = 0;
+	for(int i = 1; i < totalPages; i++){
+		if(lastUseTime[lowerIndex] > lastUseTime[i] && fixCount[i] == 0) lowerIndex = i;
+	}
+	return fixCount[lowerIndex] == 0 ? lowerIndex : -1;
+}
+
